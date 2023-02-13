e32f52a21b1cd6a75edf7ddddc42fd9702b4c03d
PhanChhayFong <phanchhayfong168@gmail.com>
2023-02-13 17:05:05 +0700

    AdminDashboard

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,18 +5,13 @@ const Pagination = ({ itemsPerPage, currentPage, totalItems, paginate }) => {
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++)
     pageNumbers.push(i);
+  
   return (
     <nav className="bg-secondary text-light m-auto w-50 text-center">
       <ul className="pagination justify-content-center">
-        <li className="page-item bg-secondary text-light"><a onClick={() => paginate(currentPage==1?currentPage:currentPage-1)} style={mouse} className={`page-link bg-secondary text-light`}><i className="fa fa-arrow-left" aria-hidden="true"></i></a></li>
-        {pageNumbers.map((number) => (
-          <li key={number} className="page-item bg-secondary text-light">
-            <a onClick={() => paginate(number)} style={mouse} className={`page-link bg-${currentPage==number?"":"secondary"} text-light`}>
-              {number}
-            </a>
-          </li>
-        ))}
-        <li className="page-item bg-secondary text-light"><a onClick={() => paginate(currentPage==pageNumbers.length?currentPage:currentPage+1)} style={mouse} className={`page-link bg-secondary text-light`}><i className="fa fa-arrow-right" aria-hidden="true"></i></a></li>
+        {currentPage==1?"":<li className="page-item bg-secondary text-light"><a onClick={() => paginate(currentPage-1)} style={mouse} className={`page-link bg-secondary text-light`}><i className="fa fa-arrow-left" aria-hidden="true"></i></a></li>}
+        {pageNumbers.map((number) => (<li key={number} className="page-item bg-secondary text-light"><a onClick={() => paginate(number)} style={mouse} className={`page-link bg-${currentPage==number?"":"secondary"} text-light`}>{number}</a></li>))}
+        {currentPage==pageNumbers.length?"":<li className="page-item bg-secondary text-light"><a onClick={() => paginate(currentPage+1)} style={mouse} className={`page-link bg-secondary text-light`}><i className="fa fa-arrow-right" aria-hidden="true"></i></a></li>}
       </ul>
     </nav>
   );
