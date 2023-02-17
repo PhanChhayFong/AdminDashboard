f75531c5210120a4e377c3680fcc1344f39fedc1
PhanChhayFong <phanchhayfong168@gmail.com>
2023-02-17 14:43:10 +0700

    AdminDashboard

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -9,7 +9,7 @@ const Pagination = ({ itemsPerPage, currentPage, totalItems, paginate }) => {
     <nav className="bg-secondary text-light m-auto w-50 text-center">
       <ul className="pagination justify-content-center">
         {currentPage==1?"":<li className="page-item bg-secondary text-light"><a onClick={() => paginate(currentPage-1)} className={`mouse page-link bg-secondary text-light`}><i className="fa fa-arrow-left" aria-hidden="true"></i></a></li>}
-        {pageNumbers.map((number) => (<li key={number} className="page-item bg-secondary text-light"><a onClick={() => paginate(number)} className={`mouse page-link bg-${currentPage==number?"":"secondary"} text-light`}>{number}</a></li>))}
+        {pageNumbers.map((number) => (<li key={number} className="page-item bg-secondary text-light"><a onClick={() => paginate(number)} className={`mouse page-link ${currentPage==number?"bg-dark":"bg-secondary"} text-light`}>{number}</a></li>))}
         {currentPage==pageNumbers.length?"":<li className="page-item bg-secondary text-light"><a onClick={() => paginate(currentPage+1)} className={`mouse page-link bg-secondary text-light`}><i className="fa fa-arrow-right" aria-hidden="true"></i></a></li>}
       </ul>
     </nav>
